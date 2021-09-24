75092acff668e5c10e4c1a812f3294cd56f71453
Dinesh Reddy <73247157+dine1717@users.noreply.github.com>
2021-09-24 22:12:52 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,5 +2,10 @@ import os
 import pandas as pd
 
 def check_data_file():
-  file ='data'
+  file ='data.zip'
   assert not os.path.exist(file), "data file is uploaded"
+
+  
+def check_data_file():
+  file ='model.h5'
+  assert not os.path.exist(file), "Model file is uploaded"
