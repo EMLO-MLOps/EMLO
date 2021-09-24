2f0ac574a436da345ce0d6606fc586a4ea2e9654
Dinesh Reddy <73247157+dine1717@users.noreply.github.com>
2021-09-24 22:23:35 +0530

    Update test.py

--- test.py
+++ test.py
@@ -23,4 +23,4 @@ def check_dog_acc():
 def check_cat_acc():
   df_met = pd.read_csv('Metrics.csv')
   acc_cat  = df_met['cat']
-  assert acc_cat > 0.7 , "Dog accuracy is less than 70%"
+  assert acc_cat > 0.7 , "Cat accuracy is less than 70%"
