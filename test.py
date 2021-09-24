a3f377627b5c2fe19acbd917e096f3cd51df677d
Dinesh Reddy <73247157+dine1717@users.noreply.github.com>
2021-09-24 22:23:23 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,21 @@ def check_data_file():
   assert not os.path.exist(file), "data file is uploaded"
 
   
-def check_data_file():
+def check_model_file():
   file ='model.h5'
   assert not os.path.exist(file), "Model file is uploaded"
+
+def check_model_acc():
+  df_met = pd.read_csv('Metrics.csv')
+  acc_new  = df_met['tot']
+  assert acc_new > 0.7 , "Overall accuracy is lessthan 70%"
+  
+def check_dog_acc():
+  df_met = pd.read_csv('Metrics.csv')
+  acc_dog = df_met['dog']
+  assert acc_dog > 0.7 , "Dog accuracy is less than 70%"
+  
+def check_cat_acc():
+  df_met = pd.read_csv('Metrics.csv')
+  acc_cat  = df_met['cat']
+  assert acc_cat > 0.7 , "Dog accuracy is less than 70%"
